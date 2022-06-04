ed08abc901e6b09224a6bb894631994c866c2439
Savithasamraj <103304359+Savithasamraj@users.noreply.github.com>
2022-06-04 18:39:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let time=10;
+setTimeout(()=>{
+document.querySelector(".count").innerText= --time;
+setTimeout(()=>{
+    document.querySelector(".count").innerText= --time;
+    setTimeout(()=>{
+        document.querySelector(".count").innerText= --time;
+        setTimeout(()=>{
+            document.querySelector(".count").innerText= --time;
+            setTimeout(()=>{
+                document.querySelector(".count").innerText= --time;
+                setTimeout(()=>{
+                    document.querySelector(".count").innerText= --time;
+                    setTimeout(()=>{
+                        document.querySelector(".count").innerText= --time;
+                        setTimeout(()=>{
+                            document.querySelector(".count").innerText= --time;
+                            setTimeout(()=>{
+                                document.querySelector(".count").innerText= --time;
+                                
+                                    setTimeout(()=>{
+                                        document.querySelector(".count").innerText="HAPPY INDEPENDENCE DAY";
+                                        
+                                        },1000);
+                                    
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
